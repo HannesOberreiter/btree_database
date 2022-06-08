d0bfc68b6b2779ee86b3cd52f080ea5e854bcb6f
HannesOberreiter <hoberreiter@gmail.com>
2022-06-08 15:08:44 +0200

    refactor: :card_file_box: Only use maria db anymore

--- init-maria/01.sql
+++ init-maria/01.sql
@@ -1,5 +1,11 @@
 CREATE DATABASE IF NOT EXISTS `beenews`;
 CREATE DATABASE IF NOT EXISTS `btree_node`;
+CREATE DATABASE IF NOT EXISTS `btree_node_test`;
 
 GRANT ALL ON `beenews`.* TO 'hannes'@'%';
 GRANT ALL ON `btree_node`.* TO 'hannes'@'%';
+GRANT ALL ON `btree_node_test`.* TO 'hannes'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'hannes'@'%';
+GRANT SELECT,LOCK TABLES ON information_schema.* TO 'hannes'@'%';
+
+FLUSH PRIVILEGES;
